8159e85b1871283bceb1e56b3a70fb67e851feca
Chris Zhou <chriszhou@ChrisdeMacBook-Pro.local>
2020-06-23 16:04:53 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-'use strict';
-import {
-    NativeModules,
-} from 'react-native';
-const {
-    AllPay
-} = NativeModules;
+'use strict'
+import { NativeModules, Platform } from 'react-native'
+const { AllPay } = NativeModules
 
-
-export const allPay = param => {
-
-    return new Promise(resolve => {
-        AllPay.pay(param).then(res => {
-            resolve(res);
-        })
-    })
-
+export const allPay = (param) => {
+  return new Promise((resolve) => {
+    if (Platform.OS === 'ios') {
+      AllPay.pay(param, (res) => {
+        resolve(res)
+      })
+    } else {
+      AllPay.pay(param).then((res) => {
+        resolve(res)
+      })
+    }
+  })
 }
